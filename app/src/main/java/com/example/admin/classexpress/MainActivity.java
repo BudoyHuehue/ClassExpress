bfc0bccfe2d98bc3754ec3d23d0da175711fd2f2
BudoyHueHue <francedamiel@yahoo.com>
2017-06-27 19:29:27 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/admin/classexpress/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.admin.classexpress;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by admin on 26/06/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "http://appgogo.orbium.com";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+    }
+}
